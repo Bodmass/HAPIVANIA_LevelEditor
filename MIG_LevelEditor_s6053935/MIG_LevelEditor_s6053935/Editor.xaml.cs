b0a70409052ddf0c6ce9e8f8cfb3f33ed10506b8
Aziz <ararwow@gmail.com>
2017-12-03 18:30:58 +0000

    Updates
    Added Editor Window

--- MIG_LevelEditor_s6053935/MIG_LevelEditor_s6053935/Editor.xaml.cs
+++ MIG_LevelEditor_s6053935/MIG_LevelEditor_s6053935/Editor.xaml.cs
@@ -10,30 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MIG_LevelEditor_s6053935
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Editor.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Editor : Window
     {
-        public MainWindow()
+        public Editor()
         {
             InitializeComponent();
-
-
-        }
-
-        private void New_Click(object sender, RoutedEventArgs e)
-        {
-            Editor win = new Editor();
-            win.Show();
-            this.Close();
         }
     }
-
-
 }

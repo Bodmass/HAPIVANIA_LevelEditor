e309e50a46d7d8b61d3ecd5b0afd06d4fe855c21
Aziz <ararwow@gmail.com>
2017-12-04 07:54:50 +0000

    Updates
    Modified Editor Window

--- MIG_LevelEditor_s6053935/MIG_LevelEditor_s6053935/Editor.xaml.cs
+++ MIG_LevelEditor_s6053935/MIG_LevelEditor_s6053935/Editor.xaml.cs
@@ -19,9 +19,63 @@ namespace MIG_LevelEditor_s6053935
     /// </summary>
     public partial class Editor : Window
     {
+        int width = 30;
+        int height = 10;
         public Editor()
         {
+
+
             InitializeComponent();
+            DisplayGrid();
+
+
+        }
+
+        private void HideDropdownGrid()
+        {
+            //Grid_Settings.Visibility = System.Windows.Visibility.Hidden;
+            //Grid_Tiles.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void Tiles_Click(object sender, RoutedEventArgs e)
+        {
+            //HideDropdownGrid();
+            //Grid_Tiles.Visibility = System.Windows.Visibility.Visible;
+        }
+
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            //HideDropdownGrid();
+            //Grid_Settings.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void DisplayGrid()
+        {
+            int size = 32;
+            int top = 0;
+            int left = 0;
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    Rectangle rect = new Rectangle();
+                    rect.Width = size; rect.Height = size;
+                    rect.Fill = Brushes.Magenta;
+
+                    Canvas.SetTop(rect, top);
+                    Canvas.SetLeft(rect, left);
+                    canvasman.Children.Add(rect);
+
+                    left += (size + 1);
+                }
+
+                left = 0;
+                top += (size + 1);
+            }
         }
     }
-}
+
+
+ }
